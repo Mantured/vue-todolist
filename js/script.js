871791925437b3fef3c2a0bde5664647c1e21a80
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-18 17:32:43 +0100

    added check and click for "done"

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
 
 /* MILESTONE 1
 Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-MILESTONE 3
 */
 
 const app = new Vue(
@@ -12,8 +9,24 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            newToDoComponent:'',
+            newToDoComponent: '',
             toDoList: [
+                {
+                    text: 'Fare to do list',
+                    done: true,
+                },
+                {
+                    text: 'Aggiungere Span',
+                    done: false,
+                },
+                {
+                    text: 'Fare to do list',
+                    done: true,
+                },
+                {
+                    text: 'Aggiungere Span',
+                    done: false,
+                },
                 {
                     text: 'Fare to do list',
                     done: true,
@@ -43,7 +56,15 @@ const app = new Vue(
             },
             removeContent(toDoIndex) {
                 (this.toDoList[toDoIndex] != undefined) ? this.toDoList.splice(toDoIndex, 1) : console.warn('Doble Nope');
+            },
+            itsDone(index) {
+                if (this.toDoList[index].done == true) {
+                    this.toDoList[index].done = false;
+                } else {
+                    this.toDoList[index].done = true;
+                }
             }
         },
+
     }
 )
